76cd792e3db4c8d629c480bc2bcd0d73e1ba6e35
ALGOSUP <pierre.gorin@algosup.com>
2025-01-08 09:56:19 +0100

    Modify Tests and Workflow name

--- code/hello_world.cpp
+++ code/hello_world.cpp
@@ -1,6 +1,7 @@
-   #include <iostream>
+// code/hello_world.cpp
+#include <iostream>
 
-   int main() {
-       std::cout << "Hello, World!" << std::endl;
-       return 0;
-   }
+int main() {
+   std::cout << "Hello, World!" << std::endl;
+   return 0;
+}
--- code/unit_tests.cpp
+++ code/unit_tests.cpp
@@ -1,14 +1,37 @@
-   #include <gtest/gtest.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+#include "hello_world.cpp"  // Include hello_world.cpp to use printHelloWorld
 
-   // A simple test case
-   TEST(SampleTest, BasicAssertions) {
-       // Check if 1 + 1 equals 2
-       EXPECT_EQ(1 + 1, 2);
-       // Check if 1 + 1 does not equal 3
-       EXPECT_NE(1 + 1, 3);
-   }
+// Test fixture to capture output
+class HelloWorldTest : public ::testing::Test {
+protected:
+    std::ostringstream captured_output;
 
-   int main(int argc, char **argv) {
-       ::testing::InitGoogleTest(&argc, argv);
-       return RUN_ALL_TESTS();
-   }
+    // Override the stream buffer of std::cout to capture printed output
+    void SetUp() override {
+        std::streambuf* original_buffer = std::cout.rdbuf();
+        std::cout.rdbuf(captured_output.rdbuf());
+    }
+
+    void TearDown() override {
+        std::cout.rdbuf(nullptr);  // Reset std::cout to original buffer
+    }
+};
+
+// Test case: check if printHelloWorld() outputs the correct string
+TEST_F(HelloWorldTest, OutputsHelloWorld) {
+    printHelloWorld();
+    EXPECT_EQ(captured_output.str(), "Hello, World!\n");
+}
+
+// Test case: check if printHelloWorld() does NOT output an incorrect string
+TEST_F(HelloWorldTest, DoesNotOutputIncorrectString) {
+    printHelloWorld();
+    EXPECT_NE(captured_output.str(), "Hello, Universe!\n");  // Deliberate incorrect string
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
