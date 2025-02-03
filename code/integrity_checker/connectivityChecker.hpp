9419a39472619597b09894d8aaea20ebf80b1053
Axel DAVID <axel.david@algosup.com>
2025-02-03 14:09:59 +0100

    save
    + need to finish checker function and I will be done

--- code/integrity_checker/connectivityChecker.hpp
+++ code/integrity_checker/connectivityChecker.hpp
@@ -3,15 +3,34 @@
 
 #include "header.hpp"
 
-int checker(DAG& dag, int node)
+void checker(DAG& dag, int node, vector<int>& visited)
 {
-    return 0;
+    if(visited[node] == 1)
+        return;
+    visited[node] = 1;
+    checker(dag, node, visited);
+}
+
+bool parseVisited(vector<int>& visited)
+{
+    for(int i : visited)
+    {
+        if(i == 0)
+            return false;
+    }
+    return true;
 }
 
 void connectivityChecker(DAG& dag) 
 {
+    bool fullyConnected;
     vector<int> visited(NODE_MAX_VALUE, 0);
-    checker(dag, 0);
+    checker(dag, 0, visited);
+    fullyConnected = parseVisited(visited);
+    if(fullyConnected == true)
+        cout << "graph is fully connected" << endl;
+    else
+        cout << "graph isn't fully connected" << endl;
 }
 
 #endif
