9a4471fc793f2c47abbbee4326d7d5f1d7bff03b
Benoît de Keyn <benoit.dekeyn@algosup.com>
2025-02-08 17:14:02 +0100

    fixed the windows launcher

--- code/windowsProjectBuilder.bat
+++ code/windowsProjectBuilder.bat
@@ -1,6 +1,4 @@
-::The script is quite simple. It first checks if the  g++  compiler is installed. If not, it installs it. 
-::Then, it compiles the  main.cpp  file using  g++  and runs the compiled executable. 
-::Finally, it opens the  index.html  file in the default browser. 
+:: The script checks if g++ is installed. If not, it installs it, compiles the code, and runs it.
 @echo off
 setlocal EnableDelayedExpansion
 
@@ -8,26 +6,73 @@ setlocal EnableDelayedExpansion
 set "SCRIPT_DIR=%~dp0"
 set "PATH_FINDER_DIR=%SCRIPT_DIR%path_finder"
 set "FRONT_DIR=%SCRIPT_DIR%front"
+set "MINGW_PATH=C:\msys64\mingw64\bin"
+set "GPP=C:\msys64\mingw64\bin\g++.exe"
 
 :: Check if g++ is installed
 where g++ >nul 2>nul
 if %errorlevel% neq 0 (
-    echo g++ compiler not found. Installing MinGW...
-    winget install -e --id MSYS2.MSYS2 >nul 2>nul
-    if %errorlevel% neq 0 (
-        echo Failed to install MinGW. Please install it manually.
-        exit /b 1
+    echo g++ compiler not found.
+
+    echo Installing MinGW...
+    winget install -e --id MSYS2.MSYS2
+    echo MinGW installed.
+
+    echo Installing g++...
+    C:\msys64\usr\bin\bash.exe -lc "pacman -S --noconfirm --needed mingw-w64-x86_64-gcc"
+    echo g++ installation completed.
+    
+    echo Adding mingw64 tools to the path
+    
+    if exist "%GPP%" (
+        :: 
+        goto path_changer
+
+    ) else (
+        echo "%GPP% not found."
+        echo Installation failed. Please install the compiler manually.
+        pause
     )
-    echo MinGW installed. Please restart your command prompt and run this script again.
-    exit /b 1
+) else (
+    echo g++ compiler found.
+    echo Proceeding with the compilation process.
+    goto compile
 )
 
+
+
+:path_changer
+
+:: Create a temporary batch file to set the PATH variable for future sessions
+echo @echo off > setpath.bat
+echo setx PATH "%MINGW_PATH%;%%PATH%%" /M >> setpath.bat
+
+:: Call the temporary batch file
+call setpath.bat
+
+:: Clean up the temporary batch file
+del setpath.bat
+
+:: Update the PATH variable for the current session
+set PATH=%MINGW_PATH%;%PATH%
+
+:: Display the updated PATH variable
+echo %PATH%
+
+
+
+:compile
+
 :: Compile the C++ code
-echo Compiling main.cpp...
-g++ -std=c++23 -O3 -o "%PATH_FINDER_DIR%\main.exe" "%PATH_FINDER_DIR%\main.cpp" -lws2_32
+echo Compiling main.cpp..
+cd %PATH_FINDER_DIR%
+@echo on
+g++ -std=c++23 -O3 -o main.exe main.cpp -lws2_32
+@echo off
 if %errorlevel% neq 0 (
     echo Compilation failed.
-    exit /b 1
+    pause
+    exit
 )
 
 echo Compilation successful. Running main.exe and opening HTML file...
